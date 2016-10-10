68040e4de02e9dde5f678e4f258b11c96429ad90
Llikdowk <guillermofm7@gmail.com>
2016-10-10 13:44:16 +0200

    car movement changed from using UPawnMovementController to custom management

--- Source/Intro1/MyVehicleMovement.cpp
+++ Source/Intro1/MyVehicleMovement.cpp
@@ -5,8 +5,6 @@
 #include "DrawDebugHelpers.h"
 
 UMyVehicleMovement::UMyVehicleMovement() {
-//    PrimaryComponentTick.bCanEverTick = true;
-//    PrimaryComponentTick.bStartWithTickEnabled = true;
 }
 
 void UMyVehicleMovement::SetThrottleInput(float InputValue) {
@@ -23,17 +21,38 @@ void UMyVehicleMovement::TickComponent(float DeltaTime, enum ELevelTick TickType
 }
 
 void UMyVehicleMovement::CalcThrottle(float DeltaTime) {
+
     FVector ForwardV = PawnOwner->GetActorForwardVector();
+
+    FVector TotalForces = FVector::ZeroVector;
+    TotalForces += -AerodynamicDrag * Velocity * Velocity.Size();
+    TotalForces += -RollingResistance * Velocity;
+    if (InputThrottle > 0) {
+        TotalForces += InputThrottle * Traction * ForwardV;
+    }
+    else {
+        TotalForces += InputThrottle * Brake * ForwardV;
+    }
+
+    
+    Acceleration = TotalForces / Mass;
+    Velocity = Velocity + Acceleration * DeltaTime;
+    FVector Position = PawnOwner->GetActorLocation();
+    FVector NewPosition = Position + Velocity * DeltaTime;
+    PawnOwner->SetActorLocation(NewPosition);
+    
     DrawDebugLine(GetWorld(), PawnOwner->GetActorLocation(), PawnOwner->GetActorLocation() + ForwardV*1000.0f, FColor::Cyan, false, -1.0f, (uint8)'\000', 5.0f);
-    DrawDebugLine(GetWorld(), PawnOwner->GetActorLocation(), PawnOwner->GetActorLocation() + GetPendingInputVector()*100.0f, FColor::Red, false, -1.0f, (uint8)'\000', 10.0f);
-    AddInputVector(ForwardV*InputThrottle*DeltaTime*Throttle);
-    int Sign = FMath::Sign(PawnOwner->GetTransform().InverseTransformVector(GetPendingInputVector()).X);
-    AddInputVector(-GetPendingInputVector().SafeNormal()*DeltaTime*Friction);
+    DrawDebugLine(GetWorld(), PawnOwner->GetActorLocation(), PawnOwner->GetActorLocation() + Velocity*100.0f, FColor::Red, false, -1.0f, (uint8)'\000', 10.0f);
+    //AddInputVector(ForwardV*InputThrottle*DeltaTime*Throttle);
+    //int Sign = FMath::Sign(PawnOwner->GetTransform().InverseTransformVector(GetPendingInputVector()).X);
+    //AddInputVector(-GetPendingInputVector().SafeNormal()*DeltaTime*Friction);
+    UE_LOG(LogTemp, Warning, TEXT("v= %s km/h"), *(FString::SanitizeFloat(Velocity.Size() * 0.01f * 3.6f)));
 
 }
 
 void UMyVehicleMovement::CalcSteering(float DeltaTime) {
     PawnOwner->AddActorLocalRotation(FQuat(FVector::UpVector, InputSteering * SteeringPower * DeltaTime), true);
+    
 }
 
 void UMyVehicleMovement::ApplyMovement(float DeltaTime) {
--- Source/Intro1/MyVehicleMovement.h
+++ Source/Intro1/MyVehicleMovement.h
@@ -26,16 +26,38 @@ public:
     virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
     void ApplyMovement(float DeltaTime);
 
+    /* DEPRECATED */
     UPROPERTY(EditAnywhere)
     float Throttle = 10.0f;
     UPROPERTY(EditAnywhere)
     float Friction = 5.0f;
     UPROPERTY(EditAnywhere)
     float SteeringPower = 2.0f;
+    /**/
+
+    UPROPERTY(EditAnywhere)
+    float Mass = 1.0f;
+
+    // forces
+    UPROPERTY(EditAnywhere)
+    float AerodynamicDrag;
+    UPROPERTY(EditAnywhere)
+    float RollingResistance;
+    UPROPERTY(EditAnywhere)
+    float Traction = 1.0;
+    UPROPERTY(EditAnywhere)
+    float Brake = 1.0;
+
+    UPROPERTY(EditAnywhere)
+    float CorneringStiffness = 1.0f;
 
 private:
     float InputThrottle;
     float InputSteering;
     void CalcThrottle(float DeltaTime);
     void CalcSteering(float DeltaTime);
+
+    FVector Velocity = FVector::ZeroVector;
+    FVector Acceleration = FVector::ZeroVector; 
+
 };
